ad1c0a9962aa9106f83a4ee83924b49ea0b5417c
Song_a <o37o19@swu.ac.kr>
2021-05-20 22:59:48 +0900

    [Add] routes user 변경

--- GreenHack/routes/users/index.js
+++ GreenHack/routes/users/index.js
@@ -56,18 +56,16 @@ router.post('/signup', async (req, res) => {
 //로그인
 router.post('/signin', async (req, res) => {
     const { email, password } = req.body;
-
     //2. request data 확인하기, email, password, userName data가 없다면 NullValue 반환
-     if(!email || !password){
-            console.log('필요한 값이 없습니다');
-            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
-        }
-
-     try {
+    if (!email || !password) {
+        console.log('필요한 값이 없습니다!');
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    }
+    try {
     //3. 존재하는 아이디인지 확인하기. 존재하지 않는 아이디면 NO USER 반환
     const alreadyEmail = await User.findOne({
-        where:{
-            email: email,
+    where: {
+        email: email,
         },
     });
     console.log(alreadyEmail);
@@ -75,37 +73,60 @@ router.post('/signin', async (req, res) => {
     console.log('없는 이메일 입니다.');
     return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
     }
-
-    //4. 비밀번호 확인하기 - 로그인할 email의 salt를 DB에서 가져와서 사용자가 request로 보낸 password와 암호화를 한후 디비에 저장되어있는 password와 일치하면 true
-    //일치하지 않으면 Miss Match password 반환
-    const { id, nickName, salt, password: hashedPassword } = alreadyEmail;
+    //4. 비밀번호 확인하기 - 로그인할 id의 salt를 DB에서 가져와서 사용자가 request로 보낸 password와 암호화를 한후 디비에 저장되어있는 password와 일치하면 true
+    // 일치하지 않으면 Miss Match password 반환
+    const { id, userName, salt, password: hashedPassword } = alreadyEmail;
     const inputPassword = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('base64');
-
-    if(inputPassword != hashedPassword){
+    if (inputPassword !== hashedPassword) {
         console.log('비밀번호가 일치하지 않습니다.');
         return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.OK, responseMessage.MISS_MATCH_PW));
     }
-
     //5. status: 200 ,message: SIGN_IN_SUCCESS, data: id, email, userName 반환
     return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.SIGN_IN_SUCCESS, { id, email, userName }));
-
-    } catch(error){
-        console.log(error);
-        return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.SIGN_IN_FAIL));
+    } catch (error) {
+    console.error(error);
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.SIGN_IN_FAIL));
     }
 })
 
 //모든 사용자 정보
 router.get('/', async (req, res) => {
-    //1. 모든 사용자 정보 (id, email, userName ) 리스폰스!
+    //1. 모든 사용자 정보 (id, email, nickName ) 리스폰스!
+    try{
+    const users = await User.findAll({
+        attributes: ['id', 'email', 'nickName'],
+    });
+    console.log(users);
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.USER_READ_ALL_SUCCESS, users));
+    }catch(error){
+        console.error(error);
+        return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.USER_READ_ALL_FAIL)));
+    }
     // status: 200, message: READ_USER_ALL_SUCCESS, data: id, email, userName 반환
 })
 
 //
 router.get('/:id', async (req, res) => {
     //1. parameter로 id값을 받아온다! (id값은 인덱스값)
+    const { id } = req.params;
     //2. id값이 유효한지 체크! 존재하지 않는 아이디면 NO_USER 반환
-    //3. status:200 message: READ_USER_SUCCESS, id, email, userName 반환
+    try{
+        const user = await User.findOne({
+            where: {
+                id: id,
+            },
+            attributes: ['id', 'email', 'nickName'],
+        });
+        if(!user) {
+            console.log('존재하지 않는 아이디 입니다.');
+            return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+        }
+        //3. status:200 message: READ_USER_SUCCESS, id, email, userName 반환
+        return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_USER_SUCCESS, user));
+       } catch (error) {
+            console.error(error);
+            return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.USER_READ_ALL_FAIL)))
+    }
 })
 
 module.exports = router;
