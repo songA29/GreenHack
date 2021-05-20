632f655c91b0846ce52ebd49d459be9374943e68
Song_a <o37o19@swu.ac.kr>
2021-05-20 17:29:19 +0900

    [Add] http-errors install

--- GreenHack/app.js
+++ GreenHack/app.js
@@ -19,10 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res){
-  res.send('Hello');
-});
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
