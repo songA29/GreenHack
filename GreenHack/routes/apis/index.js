2db152afdbea094b78ba4397c10d634c68ea540b
Song_a <o37o19@swu.ac.kr>
2021-05-21 13:39:59 +0900

    [Add] api/emissions 추가

--- GreenHack/routes/apis/index.js
+++ GreenHack/routes/apis/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const util = require('../../modules/util');
+const responseMessage = require('../../modules/responseMessage');
+const statusCode = require('../../modules/statusCode');
 
 //배출량
 router.get('/emissions', async (req, res) => {
